08aa20955213b36bce03df69e0022a310866a836
KindledFlames <kindledflames@gmail.com>
2015-12-30 18:05:50 -0600

    add test to test character class

--- character.py
+++ character.py
@@ -1,5 +1,7 @@
 #class for character for text-based game
 
+from creature import *
+
 class Character(Creature):
 	def __init__(self, pHealth, pStrength):
 		super(Character, self).__init__(pHealth, pStrength)
---
+++ test.py
@@ -0,0 +1,8 @@
+#test file to test character class
+
+from creature import *
+from character import *
+
+testcharacter = Character(4, 5)
+print testcharacter.health
+print testcharacter.strength
